0c865478602a0017aab854bdf3b75c9e5558a106
GToshack <graydon.toshack@gmail.com>
2024-11-09 01:12:55 -0500

    added base site - non functional

---
+++ main.js
@@ -0,0 +1,12 @@
+let resources = document.getElementById("resources")
+resources.innerText = "-"
+
+function dropdown(num) {
+    const text = {
+        1 : "Resources -",
+        2 : "Calculators -",
+        3 : "Other Sites -"
+    }
+
+    
+}
